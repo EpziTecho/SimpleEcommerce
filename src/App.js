aae4361bcfd2f2a2c9732de3cec40f6eb5ad941f
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-03-23 14:12:32 -0500

    Modelando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react'
 function App() {
+   //Aqui se va a consumir el servicio de categorias mediante fetchAPI y se va a mostrar en el navegador 
+  const [results,setResults] = useState([]);
+  React.useEffect(() => {
+
+    fetch("http://localhost:3001/categorias").then(response => response.json()).then(data => {
+      console.log(data);
+      setResults(data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <header>Simple Ecommerce de Frutas</header>
+
+    <section>
+      <nav>
+      {
+      results.map(d => (
+      <div key={d.id} >{d.nombre}</div>
+      ))
+      }
+      </nav>
+      <article>
+        Area Principal
+      </article>
+    </section>
+   <footer></footer>
+      </>
   );
+  
 }
 
 export default App;
